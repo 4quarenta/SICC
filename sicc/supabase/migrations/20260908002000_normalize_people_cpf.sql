8e18069da9b81c17b0938bcd4ccf95f4f5af30a7
4quarenta <70922568+4quarenta@users.noreply.github.com>
2026-09-07 15:25:21 -0300

    Normaliza CPF e impede formatos inválidos

---
+++ sicc/supabase/migrations/20260908002000_normalize_people_cpf.sql
@@ -0,0 +1,14 @@
+begin;
+
+update public.people
+set cpf = regexp_replace(cpf, '\\D', '', 'g')
+where cpf <> regexp_replace(cpf, '\\D', '', 'g');
+
+alter table public.people
+  drop constraint if exists people_cpf_digits_check;
+
+alter table public.people
+  add constraint people_cpf_digits_check
+  check (cpf ~ '^[0-9]{11}$');
+
+commit;
